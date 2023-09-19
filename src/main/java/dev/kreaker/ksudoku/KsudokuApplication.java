0292a5fd837bfc290ecd27f6bfe5146e2dabc32a
Alejandro Lopez Monzon <alexlm78@gmail.com>
2023-09-19 16:38:02 -0600

    initilized project with spring/gradle

---
+++ src/main/java/dev/kreaker/ksudoku/KsudokuApplication.java
@@ -0,0 +1,13 @@
+package dev.kreaker.ksudoku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsudokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsudokuApplication.class, args);
+	}
+
+}
